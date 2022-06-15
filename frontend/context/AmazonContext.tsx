3662bea5577bf83093baaaf1ec7259803d7b5678
olehstrokan <oleh@wattstor.com>
2022-06-15 20:19:25 +0200

    Wrapped app, updated context

--- frontend/context/AmazonContext.tsx
+++ frontend/context/AmazonContext.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import { useMoralis } from 'react-moralis';
 
 
 interface IAmazonContext {
@@ -7,7 +8,17 @@ oleh: number
 export const AmazonContext = React.createContext<IAmazonContext | null>(null);
 
 export const AmazonProvider:React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [username, setUsername] = React.useState<string>('')
+  const [nickname, setNickname] = React.useState<string>('');
 
+  const {
+    authenticate,
+    isAuthenticated,
+    enableWeb3,
+    Moralis,
+    user,
+    isWeb3Enabled,
+  } = useMoralis()
 
   return (
     <AmazonContext className="Provider" value={{
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis';
+import { AmazonProvider } from '../context/AmazonContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID} initializeOnMount={false}>
-    <Component {...pageProps} />
+    <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+                     initializeOnMount={false}>
+      <AmazonProvider>
+        <Component {...pageProps} />
+      </AmazonProvider>
     </MoralisProvider>
   )
 }
