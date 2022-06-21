3925ea064bf6c476ecd9b7cc7f0ad563d4ec2403
olehstrokan <oleh@wattstor.com>
2022-06-21 17:43:22 +0200

    Added cards component

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import { AmazonContext } from '../context/AmazonContext';
+import { FaCoins } from 'react-icons/all';
+
+interface ICard {
+  item: any
+}
 
 
-export const Cards = () => {
+export const Card:React.FC<ICard> = ({ item }) => {
   const styles = {
-    container: `h-full w-full flex flex-col ml-[20px] -mt-[50px]`,
-    title: `text-xl font-bolder mb-[20px] mt-[30px]  ml-[30px]`,
-    cards: `flex items-center  flex-wrap gap-[80px]`,
+    cardContainer: `flex flex-col`,
+    card: `h-[250px] w-[190px] rounded-3xl flex cursor-pointer transition-all duration-300  hover:scale-105 hover:shadow-xl overflow-hidden border border-black shadow-xl border-4 border-[#fb9701]`,
+    cardTitle: `text-xl font-bold flex text-center w-full flex-1 justify-center mt-[10px]`,
+    price: `text-md font-bold flex justify-center`,
+    coins: `ml-[10px]`,
   }
-  const { assets } = React.useContext(AmazonContext)
-
+  const { buyAsset } = React.useContext(AmazonContext)
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>New Release</div>
-      <div className={styles.cards}>
-        <div className={styles.cards}>
-          {assets.map((item) => {
-            let asset = item.attributes
-
-            return <Card key={item.id} item={item.attributes} />
-          })}
+    <div
+      className={styles.cardContainer}
+      onClick={() => buyAsset(item.price, item)}
+    >
+      <div className={styles.card}>
+        <Image
+          src={item.src}
+          className='object-cover object-center'
+          width={190}
+          height={250}
+        />
+      </div>
+      <div>
+        <div className={styles.cardTitle}>{item.name}</div>
+        <div className={styles.price}>
+          {item.price} AC <FaCoins className={styles.coins} />
         </div>
       </div>
     </div>
