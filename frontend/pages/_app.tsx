d0ffdae0163829eecd4ba3ccaba05186638bfc61
olehstrokan <oleh@wattstor.com>
2022-06-26 19:41:02 +0200

    Added Header component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis';
 import { AmazonProvider } from '../context/AmazonContext';
+import { ModalProvider } from 'react-simple-hook-modal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
                      initializeOnMount={false}>
       <AmazonProvider>
+        <ModalProvider>
         <Component {...pageProps} />
+        </ModalProvider>
       </AmazonProvider>
     </MoralisProvider>
   )
